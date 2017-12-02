4a3ca6101bd928ca76f1d4abb5807a6d73c2218c
hlebon <hanslgarcia@gmail.com>
2017-12-02 06:11:54 -0600

    feat: back button in addTask page

--- src/component/AddPage/AddTask.js
+++ src/component/AddPage/AddTask.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class AddTask extends React.Component{
     state = {
@@ -37,6 +38,9 @@ class AddTask extends React.Component{
                         placeholder="Description" />
                 </div>
                 <div className="form-group">
+                    <Link to="/" className="btn btn-secondary">
+                        <span>Back</span>
+                    </Link>
                     <button className="btn" type="submit">
                         <span>Create</span>
                     </button>

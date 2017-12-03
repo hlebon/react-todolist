f90c0db8eed570cecfa236941ed85177acc74776
hlebon <hanslgarcia@gmail.com>
2017-12-02 22:04:48 -0600

    feat: retouch

--- src/App.js
+++ src/App.js
@@ -3,35 +3,37 @@ import TodoList from './component/HomePage/TodoList'
 import AddTask from './component/AddPage/AddTask'
 import { Route } from 'react-router-dom'
 
-
 const Todo = [
   {
     id: 1,
     title: "Hacer Super",
     description: "Comprar para el desayuno de mañana y el almuerzo",
-    author: "Hans",
-    creationDate: "14/11/2017",
-    complete: false
+    creationDate: (new Date("2017-10-2")).toLocaleDateString(),
+    completed: false
   },
   {
     id: 2,
     title: "Pintar la casa",
     description: "LLamar para preguntar a que hora puedo pasar por la pintura",
-    author: "Daniel",
-    creationDate: "25/10/2017",
-    complete: true
+    creationDate: (new Date("2017-8-7")).toLocaleDateString(),
+    completed: true
   },
   {
     id: 3,
     title: "Estudiar para examen",
     description: "Estudiar los ultimos dos capitulos PMBOOK",
-    author: "Claudia",
-    creationDate: "8/11/2017",
-    complete: false
+    creationDate: (new Date("2017-11-3")).toLocaleDateString(),
+    completed: false
   }
 ]
 
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
 
+  return result;
+};
 
 class App extends Component{
   state = {
@@ -43,6 +45,25 @@ class App extends Component{
       list: Todo
     })
   }
+
+  onDragEnd = (result) => {
+    console.log(result)
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const list = reorder(
+      this.state.list,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      list,
+    });
+  }
+
 
   handleAddTask = (task) => {
     task["id"] = this.state.list.length + 1
@@ -62,11 +83,22 @@ class App extends Component{
     }))
   }
 
+  onCompleteTask = (objItem) => {
+    this.setState((prevState) => ({
+      list: prevState.list.map((item)=>{
+        if(item.id === objItem.id){
+          item.completed = !item.completed
+        }
+        return item
+      })
+    }))
+  }
+
   render(){
     return(
       <div className="container">
         <Route exact path="/" render={()=> (
-          <TodoList list={this.state.list} onRemoveTask={this.onRemoveTask}/>
+          <TodoList onDragEnd={this.onDragEnd} list={this.state.list} onRemoveTask={this.onRemoveTask} onCompleteTask={this.onCompleteTask}/>
         )}/>
         <Route path="/add" render={({history})=> (
           <AddTask addTask={(task) => {
--- src/component/HomePage/TodoList.js
+++ src/component/HomePage/TodoList.js
@@ -2,51 +2,59 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import escapeRegExp from 'escape-string-regexp'
 import PropTypes from 'prop-types'
+import sortBy from 'sort-by'
 import MdAdd from 'react-icons/lib/md/add'
 import MdSearch from 'react-icons/lib/md/search'
 import MdDelete from 'react-icons/lib/md/delete'
 import MdKeyboardArrowUp from 'react-icons/lib/md/keyboard-arrow-up'
+import MdCheckBox from 'react-icons/lib/md/check-box'
+import MdCheckBoxOutlineBlank from 'react-icons/lib/md/check-box-outline-blank'
 
 function Item(props){
   return (
     <div className="cflex-wrap">
       <div className="cfw-author center-item">
-          {props.item.author[0].toUpperCase()}
+          {props.item.title[0].toUpperCase()}
       </div>
       <div className="cfw-content cflex-column">
-        <div>{props.item.title}</div>
+        <div className={props.item.completed ? "checked" : ""}>{props.item.title}</div>
         <small>{props.item.creationDate}</small>
       </div>
       <div className="cfw-options center-item">
         <span onClick={() => props.removeTask(props.item.id)}>
           <MdDelete/>
         </span>
+        <span onClick={() => props.completeTask(props.item)}>
+          {props.item.completed ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+        </span>
       </div>
-  </div>
-  )
+    </div>
+    )
 }
 
 Item.propTypes = {
   item: PropTypes.object.isRequired,
-  removeTask: PropTypes.func.isRequired
+  removeTask: PropTypes.func.isRequired,
+  completeTask: PropTypes.func.isRequired
 }
 
 function ItemList(props) {
   return (
     <div>
-      <ul className="list">
-        {props.list.map((item, index) => (
+        <ul className="list">
+        {props.list.map((item, index) => (  
           <li className="item-list" key={item.id}>
-            <Item item={item} removeTask={props.removeTask}/>
+            <Item item={item} removeTask={props.removeTask} completeTask={props.completeTask}/>  
           </li>
         ))}
-      </ul>
+        </ul>
     </div>)
 }
 
 ItemList.propTypes = {
   list: PropTypes.array.isRequired,
-  removeTask: PropTypes.func.isRequired
+  removeTask: PropTypes.func.isRequired,
+  completeTask: PropTypes.func.isRequired
 }
 
 function EmptyListMessage(props){
@@ -71,20 +79,11 @@ class TodoList extends Component {
         })
     }
 
-    onCreateTaks = () => {
-      alert("Hola desde onCreate Task")
-    }
-
-    showInputSearch = () => {
-      this.setState({
-        onSearch: true
-      })
-    }
-
+    showInputSearch = () =>  this.setState({onSearch: true})
     hideInputSearch = () => this.setState({ onSearch: false })
   
     render() {
-        const { list, onRemoveTask } = this.props;
+        const { list, onRemoveTask, onCompleteTask } = this.props;
         const { value, onSearch } = this.state;
 
         let newList
@@ -94,6 +93,8 @@ class TodoList extends Component {
         }else{
           newList = list
         }
+
+        newList.sort(sortBy("-id"))
         
         return (
           <div className="cflex-column">
@@ -109,7 +110,6 @@ class TodoList extends Component {
                   ? <MdKeyboardArrowUp onClick={this.hideInputSearch}/>
                   : <MdSearch onClick={this.showInputSearch}/>
                 }
-                
               </div>
             </div>
             {this.state.onSearch && 
@@ -120,7 +120,7 @@ class TodoList extends Component {
               </div>
             }
             {newList.length 
-            ? <ItemList list={newList} removeTask={onRemoveTask} />
+            ? <ItemList onDragEnd={this.props.onDragEnd} list={newList} removeTask={onRemoveTask} completeTask={onCompleteTask}/>
             : <EmptyListMessage/>}
             
           </div>
@@ -130,7 +130,8 @@ class TodoList extends Component {
 
     TodoList.propTypes = {
       list: PropTypes.array.isRequired, 
-      onRemoveTask: PropTypes.func.isRequired
+      onRemoveTask: PropTypes.func.isRequired,
+      onCompleteTask: PropTypes.func.isRequired
     }  
 
 export default TodoList;
